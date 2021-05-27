c2478356b34fb6f3c4562079c9b87594f890025a
nororo29 <wlgus4401@gmail.com>
2021-05-27 18:32:35 +0900

    Update orderForm.java

--- eclipse-workspace/Java_basic/src/java_basic_day9/orderForm.java
+++ eclipse-workspace/Java_basic/src/java_basic_day9/orderForm.java
@@ -1,5 +1,7 @@
 package java_basic_day9;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,22 +14,25 @@ public class orderForm {
 	String address;
 	
 	public void printOrder() {
-		String dateString = new SimpleDateFormat("yyyy년 MM월 dd일").format(date);
+//		String dateString = new SimpleDateFormat("yyyy년 MM월 dd일").format(date);
+		DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
 		
 		System.out.printf("주문 번호 : %.0f\n", orderId);
 		System.out.println("주문자 아이디 : " + clientId);
-		System.out.println("주문 날짜 : " + dateString);
+//		System.out.println("주문 날짜 : " + dateString);
+		System.out.println("주문 날짜 : " + df.format(date));
 		System.out.println("주문자 이름 : " + clientName);
 		System.out.println("주문 상품 번호 : " + orderNum);
 		System.out.println("배송 주소 : " + address);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		orderForm o1 = new orderForm();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		
 		o1.orderId = 201803120001d;
 		o1.clientId = "abc123";
-		o1.date = new Date();
+		o1.date = df.parse("2018-10-3");
 		o1.clientName = "홍길순";
 		o1.orderNum = "PD0345-12";
 		o1.address = "서울시 영등포구 여의도동 20번지";
